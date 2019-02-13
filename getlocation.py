73534f81a655fc838850298560a38a0301b5d1ad
Joel Jogy <joeljogy07@gmail.com>
2019-02-13 13:58:53 +0530

    Add files via upload

---
+++ getlocation.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+
+def getLocation():
+    chrome_options = Options()
+    chrome_options.add_argument("--use-fake-ui-for-media-stream")
+    timeout = 20
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get("https://mycurrentlocation.net/")
+    wait = WebDriverWait(driver, timeout)
+    longitude = driver.find_elements_by_xpath('//*[@id="longitude"]')
+    longitude = [x.text for x in longitude]
+    longitude = str(longitude[0])
+    latitude = driver.find_elements_by_xpath('//*[@id="latitude"]')
+    latitude = [x.text for x in latitude]
+    latitude = str(latitude[0])
+    driver.quit()
+    return (latitude,longitude,'100')
+
